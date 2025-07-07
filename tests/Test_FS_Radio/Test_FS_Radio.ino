2bbcb029a69154689310b1bb69e046254e2c7455
alexlynd <22926680+AlexLynd@users.noreply.github.com>
2025-07-06 23:26:25 -0700

    update pins

--- tests/Test_FS_Radio/Test_FS_Radio.ino
+++ tests/Test_FS_Radio/Test_FS_Radio.ino
@@ -10,17 +10,21 @@ Test Radio function and LEDS
 #define LED_PIN D0
 
 // Uplink
-#define SX_UL_NSS 2
-#define SX_UL_BUSY 15
+#define SX_UL_NSS D1
+#define SX_UL_BUSY 39
+#define SX_UL_IO -1
+#define SX_UL_RST -1
 
 // Downlink
-#define SX_DL_NSS 3
-#define SX_DL_BUSY 16
+#define SX_DL_NSS D2
+#define SX_DL_BUSY 38
+#define SX_DL_IO -1
+#define SX_DL_RST -1
 
 //                                (NSS, DIO1, RESET, BUSY)
 // IDK about the DIO1 and Reset pins
-SX1262 radioUplink    = new Module(SX_UL_NSS, D1, D2, SX_UL_BUSY);
-SX1262 radioDownlink  = new Module(SX_DL_NSS, D1, D2, SX_DL_BUSY);
+SX1262 radioUplink    = new Module(SX_UL_NSS, SX_UL_IO, -1, SX_UL_BUSY);
+SX1262 radioDownlink  = new Module(SX_DL_NSS, SX_DL_IO, -1, SX_DL_BUSY);
 
 void setup() {
   Serial.begin(115200);
