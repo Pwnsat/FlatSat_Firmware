76aacdaa7847b1b6aeccbb30e8127234a3e51705
JahazielLem <jahaziel.lem@gmail.com>
2025-07-20 18:03:21 -0600

    refactor: Change the firmware test for sensor and radio

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,42 +1,21 @@
-/*
-FLatSat V1 - Firmware with Telemetry collection and LoRa communication
-  PWNSAT 2025
-  Kevin Leon
+/* FlatSat Test firmware for LoRa and Sensors Telemetry
+  July 2025
 */
-
+#include <RadioLib.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
-#include <RadioLib.h>
+
 #include <Wire.h>
 
 #define FIRMWARE_VERSION "0.0.1"
 
-// Telemetry Radio
-#define FSAT_TM_NSS   D2
-#define FSAT_TM_RST   -1
-#define FSAT_TM_IO    -1
-#define FSAT_TM_BUSY  D12
-// Telecommands Radio
-#define FSAT_TC_NSS   D1
-#define FSAT_TC_RST   -1
-#define FSAT_TC_IO    -1
-#define FSAT_TC_BUSY  D11
-// Board Common Comms
-#define FSAT_SCK      D8
-#define FSAT_MISO     D9
-#define FSAT_MOSI     D10
+#define SX_NSS_1  D1
+#define SX_BUSY_1 D11
+#define SX_NSS_2  D2
+#define SX_BUSY_2 D12
 // Sensor Specific
 #define SEALEVELPRESSURE_HPA (1013.25)
-// Radio Config
-#define RADIO_FREQ  916  // Frequency
-#define RADIO_SP    12   // Spreading Factor
-#define RADIO_BW    250  // Bandwidth
-#define RADIO_CR    5    // Coding Rate
-#define RADIO_PL    8    // Preamble Length
-#define RADIO_SW    0x12 // SyncWord
-#define RADIO_OP    4    // Output Power
-#define RADIO_RECV_TIMEOUT 500
 // Telemetry Structure
 #define SPACECRAFT_ID "COSMOS1"
 #define TM_SEPARATOR ","
@@ -64,82 +43,9 @@ radio_context_t radioContext;
 Adafruit_MPU6050 mpu;
 Adafruit_BME280 bme;
 
-SPIClass spiRadio(HSPI);
-SX1262 radioTm = new Module(FSAT_TM_NSS, FSAT_TM_IO, FSAT_TM_RST, FSAT_TM_BUSY, spiRadio);
-SX1262 radioTc = new Module(FSAT_TC_NSS, FSAT_TC_IO, FSAT_TC_RST, FSAT_TC_BUSY, spiRadio);
+SX1262 uplink = new Module(SX_NSS_1, SX_BUSY_1, -1, -1);
+SX1262 downlink = new Module(SX_NSS_2, SX_BUSY_2, -1, -1);
 
-
-static void checkRadioTmSPI(){
-  Serial.println("[SYS - Radio - SPI] Testing Telemetry Communication");
-  pinMode(FSAT_TM_NSS, OUTPUT);
-  digitalWrite(FSAT_TM_NSS, HIGH);
-  spiRadio.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(FSAT_TM_NSS, LOW);
-  uint8_t response = spiRadio.transfer(0x42);
-  digitalWrite(FSAT_TM_NSS, HIGH);
-  spiRadio.endTransaction();
-
-  Serial.print("[SYS - Radio - SPI] Response: 0x");
-  Serial.println(response, HEX);
-}
-
-static void checkRadioTcSPI(){
-  Serial.println("[SYS - Radio - SPI] Testing Telecommand Communication");
-  pinMode(FSAT_TC_NSS, OUTPUT);
-  digitalWrite(FSAT_TC_NSS, HIGH);
-  spiRadio.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(FSAT_TC_NSS, LOW);
-  uint8_t response = spiRadio.transfer(0x42);
-  digitalWrite(FSAT_TC_NSS, HIGH);
-  spiRadio.endTransaction();
-
-  Serial.print("[SYS - Radio - SPI] Response: 0x");
-  Serial.println(response, HEX);
-}
-
-static int initRadioTM(){
-  Serial.println("[SYS - Radio] Telemetry Radio Init");
-
-  int state = radioTm.begin();
-  radioTm.setFrequency(RADIO_FREQ);
-  radioTm.setSpreadingFactor(RADIO_SP);
-  radioTm.setBandwidth(RADIO_BW);
-  radioTm.setCodingRate(RADIO_CR);
-  radioTm.setPreambleLength(RADIO_PL);
-  radioTm.setSyncWord(RADIO_SW);
-  radioTm.setOutputPower(RADIO_OP);
-  
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println("[SYS - Radio] Telemetry OK");
-    return RADIOLIB_ERR_NONE;
-  } else {
-    Serial.print("[SYS - Radio] Telemetry Error radio: ");
-    Serial.println(state);
-    return state;
-  }
-}
-
-static int initRadioTC(){
-  Serial.println("[SYS - Radio] Telecommand Radio Init");
-  
-  int state = radioTc.begin();
-  radioTc.setFrequency(RADIO_FREQ);
-  radioTc.setSpreadingFactor(RADIO_SP);
-  radioTc.setBandwidth(RADIO_BW);
-  radioTc.setCodingRate(RADIO_CR);
-  radioTc.setPreambleLength(RADIO_PL);
-  radioTc.setSyncWord(RADIO_SW);
-  radioTc.setOutputPower(RADIO_OP);
-  
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println("[SYS - Radio] Telecommand OK");
-    return RADIOLIB_ERR_NONE;
-  } else {
-    Serial.print("[SYS - Radio] Telecommand Error radio: ");
-    Serial.println(state);
-    return state;
-  }
-}
 
 static sensor_status_t initMPU(){
   Serial.println("[SYS - MPU] MPU6050 Init");
@@ -210,23 +116,9 @@ static String readBME(){
 
 static String getSensorsTelemetry(){
   String mpuTelemetry = readMPU();
-  // String bmeTelemetry = readBME();
-  String telemetry = mpuTelemetry; // + SENSOR_SEPARATOR + bmeTelemetry;
+  String bmeTelemetry = readBME();
+  String telemetry = mpuTelemetry;// + SENSOR_SEPARATOR + bmeTelemetry;
   return telemetry;
-}
-
-static void radioTelemetryTransmit(String payload){
-  String telemetry = String(SPACECRAFT_ID) + SENSOR_SEPARATOR + payload;
-
-  Serial.print("[SYS - Radio] Transmiting: ");
-  Serial.println(telemetry);
-  int state = radioTm.transmit("Hello");
-  if (state != RADIOLIB_ERR_NONE){
-    Serial.print("[SYS - Radio] Transmition Failed: ");
-    Serial.println(state);
-    return;
-  }
-  Serial.println("[SYS - Radio] Transmition OK");
 }
 
 static void printStringHexDump(const String& input) {
@@ -263,71 +155,77 @@ static void printStringHexDump(const String& input) {
 }
 
 
-static void radioTelecommandListen(){
-  radioTc.startReceive(RADIO_RECV_TIMEOUT);
-  String data;
-  int state = radioTc.readData(data);
-  if (state == RADIOLIB_ERR_NONE) {
-    if (data.length() == 0) return;
-    
-    Serial.println("[SYS - Radio] Telecommand Packet Received: ");
-    Serial.print("[SYS - Radio] Packet RSSI: ");
-    Serial.println(radioTc.getRSSI());
-    Serial.print("[SYS - Radio] Packet SNR: ");
-    Serial.println(radioTc.getSNR());
-    printStringHexDump(data);
-  } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-    Serial.println("[SYS - Radio] Telecommand Packet CRC Error");
-  } else {
-    Serial.print("[SYS - Radio] Telecommand Error:");
-    Serial.println(state);
-  }
-}
-
-static void sensorIntegrityCheck(){
-  if(sensorContext.mpuStatus == SENSOR_FAIL){
-    Serial.println("[SYS - SIC] MPU failed");
-    sensorContext.mpuStatus = initMPU();
-  }
-  if(sensorContext.bmeStatus == SENSOR_FAIL){
-    Serial.println("[SYS - SIC] BME failed");
-    sensorContext.bmeStatus = initBME();
-  }
-
-  if(radioContext.tmStatus == RADIOLIB_ERR_NONE){
-    Serial.println("[SYS - SIC] Radio TM failed");
-    radioContext.tmStatus = initRadioTM();
-  }
-  if(radioContext.tcStatus == RADIOLIB_ERR_NONE){
-    Serial.println("[SYS - SIC] Radio TC failed");
-    radioContext.tcStatus = initRadioTC();
-  }
-}
-
-void setup(){
+void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  while(!Serial);
 
   Serial.print("\nFirmware version: ");
   Serial.println(FIRMWARE_VERSION);
-  Serial.println("[SYS] Init sensors...");
-
-  checkRadioTmSPI();
-  checkRadioTcSPI();
-
-  spiRadio.begin(FSAT_SCK, FSAT_MISO, FSAT_MOSI);
   
+
+  Serial.println("[SYS] Radio Uplink init");
+  int state = uplink.begin();
+  if (state== RADIOLIB_ERR_NONE){
+    Serial.println("[SYS] Radio Uplink OK");
+  }else{
+    Serial.print("[SYS] Radio Uplink Error: ");
+    Serial.println(state);
+  }
+  uplink.setFrequency(916);
+  uplink.setBandwidth(250);
+  uplink.setSpreadingFactor(12);
+  uplink.setPreambleLength(8);
+  uplink.setCodingRate(5);
+
+  Serial.println("[SYS] Radio Downlink init");
+  state = downlink.begin();
+  if (state== RADIOLIB_ERR_NONE){
+    Serial.println("[SYS] Radio Uplink OK");
+  }else{
+    Serial.print("[SYS] Radio Downlink Error: ");
+    Serial.println(state);
+  }
+  downlink.setFrequency(916);
+  downlink.setBandwidth(250);
+  downlink.setSpreadingFactor(12);
+  downlink.setPreambleLength(8);
+  downlink.setCodingRate(5);
+
+  downlink.startReceive();
+
+  Serial.println("[SYS] Init sensors...");
   sensorContext.mpuStatus = initMPU();
   sensorContext.bmeStatus = initBME();
-
-  radioContext.tmStatus = initRadioTM();
-  radioContext.tcStatus = initRadioTC();
 }
 
-void loop(){
-  // sensorIntegrityCheck();
-  radioTelemetryTransmit(getSensorsTelemetry());
-  delay(1000);
-  radioTelecommandListen();
-  delay(5000);
+void loop() {
+  String telemetry = getSensorsTelemetry();
+  Serial.print("[SYS - Radio] Transmiting: ");
+  Serial.println(telemetry);
+  int state = uplink.transmit(telemetry);
+  if (state == RADIOLIB_ERR_NONE){
+    Serial.print("[SYS - Radio] Transmit: ");
+    Serial.println(telemetry);
+  }else{
+    Serial.print("[SYS - Radio] Transmit Error: ");
+    Serial.println(state);
+  }
+  delay(2000);
+  String data;
+  state = downlink.readData(data);
+  if (state == RADIOLIB_ERR_NONE){
+    Serial.println();
+    Serial.print("[SYS - Radio] Recv: ");
+    Serial.println(data);
+    Serial.print("[SYS - Radio] RSSI: ");
+    Serial.println(downlink.getRSSI());
+    Serial.print("[SYS - Radio] SNR: ");
+    Serial.println(downlink.getSNR());
+    printStringHexDump(data);
+    Serial.println();
+  }else{
+    Serial.print("[SYS - Radio] Recv Error: ");
+    Serial.println(state);
+  }
+  delay(2000);
 }
